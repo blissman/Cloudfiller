d705848e54765dba62322525c503ce5f9edb7702
blissman <blissman@gmail.com>
2016-03-02 17:22:24 -0500

    remove users index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :user_sessions
 
-  resources :users do
+  resources :users, except: :index do
     resources :experiences
     resources :requests
     resources :messages
