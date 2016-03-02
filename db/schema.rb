57372cd524b7584a154be6d1ee9ee293085c6cbf
blissman <blissman@gmail.com>
2016-03-02 16:26:41 -0500

    add UserSessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302205938) do
+ActiveRecord::Schema.define(version: 20160302212438) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 20160302205938) do
   end
 
   create_table "experiences", force: :cascade do |t|
-    t.text     "description"
-    t.string   "experience_type"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "job"
+    t.text     "education"
+    t.text     "knowledge"
   end
 
   create_table "messages", force: :cascade do |t|
