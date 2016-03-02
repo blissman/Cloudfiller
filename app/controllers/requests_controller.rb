324481d6ce615142235271234a57cc7cbfe8768f
frances conley wood <f.conleywood@gmail.com>
2016-03-02 13:48:49 -0500

    Make request actions

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,23 @@
 class RequestsController < ApplicationController
 
   def index
-
+    @requests = Request.all
   end
 
   def show
-
+    @request = Request.find(params[:id])
   end
 
   def new
-
+    @request = Request.new
   end
 
   def create
-
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to category_request_path
+    else
+      render :new
   end
 
   def edit
@@ -25,7 +29,16 @@ class RequestsController < ApplicationController
   end
 
   def destroy
-
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to category_path
   end
+
+
+private
+
+def request_params
+  params.require(:request).permit(:description, :expire, :points)
+end
 
 end
