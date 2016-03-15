6aa4f3f6e0ddf1c7ac9435b1e86131f6679a492a
blissman <blissman@gmail.com>
2016-03-15 10:25:52 -0400

    add total_ratings to user for ratings calculations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302212438) do
+ActiveRecord::Schema.define(version: 20160315142459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160302212438) do
     t.datetime "updated_at",       null: false
     t.string   "crypted_password"
     t.string   "salt"
+    t.integer  "total_ratings"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
