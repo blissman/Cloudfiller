4f3a42f7042c95f72dcf4fdf4c92f8724642002c
blissman <blissman@gmail.com>
2016-03-15 16:50:04 -0400

    remove successful login notice

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:root, notice: 'Login successful')
+      redirect_back_or_to(:root)
     else
       flash.now[:alert] = 'Invalid Username or Password'
       render action: 'new'
