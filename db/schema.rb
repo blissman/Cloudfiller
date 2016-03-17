bf56189097ce8b0b4e04b0c102b76975267e9f48
blissman <blissman@gmail.com>
2016-03-17 09:36:14 -0400

    remove messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315142459) do
+ActiveRecord::Schema.define(version: 20160317133442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,6 @@ ActiveRecord::Schema.define(version: 20160315142459) do
     t.text     "job"
     t.text     "education"
     t.text     "knowledge"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "response_id"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "requests", force: :cascade do |t|
