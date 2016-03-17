ba72ef4798b2aae1403962eb4a7ede510818fb90
blissman <blissman@gmail.com>
2016-03-17 10:07:41 -0400

    add error display and validations to the new request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,4 +6,9 @@ class Request < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :responses
+
+  validates :category_id, presence: true
+  validates :description, presence: true
+  validates :expire, presence: true
+  validates :points, presence: true
 end
