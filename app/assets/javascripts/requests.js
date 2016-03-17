b7273499a51a1f183c94e7feb7b7591b485d2861
frances conley wood <f.conleywood@gmail.com>
2016-03-17 11:48:55 -0400

    Fix ajax uncomment

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,12 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// $(document).on('ready page:load', function() {
-//   $("#done_request_btn > a").on("click",function(event) {
-//      event.preventDefault();
-//       $.ajax ({
-//       method: "POST",
-//       url: $(this).attr("href"),
-//       data: { request: {active: false}, _method: "patch" }
-//     });
-//   });
-// });
+$(document).on('ready page:load', function() {
+  $("#done_request_btn > a").on("click",function(event) {
+     event.preventDefault();
+      $.ajax ({
+      method: "POST",
+      url: $(this).attr("href"),
+      data: { request: {active: false}, _method: "patch" }
+    });
+  });
+});
