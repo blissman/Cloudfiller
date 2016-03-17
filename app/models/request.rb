96a625d7519a04313559d0cbdd7aa68a5df30d28
blissman <blissman@gmail.com>
2016-03-17 13:58:25 -0400

    add validation for points

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,9 +3,16 @@ class Request < ActiveRecord::Base
     self.active = true
   end
 
+  after_create do
+    self.user.points = self.user.points - self.points
+    self.user.save
+  end
+
   belongs_to :user
   belongs_to :category
   has_many :responses
 
-  validates :category_id, :description, :expire, :points, presence: true
+  validates :category_id, :description, presence: true
+  validates :expire,
+  validates :points, numericality: { only_integer: true, :greater_than_or_equal_to 1, :less_than_or_equal_to self.user.points }
 end
