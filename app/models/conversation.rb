5375dadf2972482b200582fef15406b69f3c0352
blissman <blissman@gmail.com>
2016-03-17 09:46:05 -0400

    modify Conversation model relationship syntax

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ActiveRecord::Base
-  belongs_to :sender, :foreign_key => :sender_id, class_name: ‘User’
-  belongs_to :recipient, :foreign_key => :recipient_id, class_name: ‘User’
+  belongs_to :user, :foreign_key => :sender_id
+  belongs_to :user, :foreign_key => :recipient_id
   has_many :messages, dependent: :destroy
 
   validates_uniqueness_of :sender_id, :scope => :recipient_id
