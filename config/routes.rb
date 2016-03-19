80811edcccd0e31db9127befc33b90e1072a4f81
blissman <blissman@gmail.com>
2016-03-19 15:19:23 -0400

    add password_resets resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'password_resets/create'
-
-  get 'password_resets/edit'
-
-  get 'password_resets/update'
 
   root "categories#index"
+
+  resources :password_resets
 
   resources :pages, only: [:show]
 
