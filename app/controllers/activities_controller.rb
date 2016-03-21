349be4362ca1351c1b4f35b4034ad6bde56633da
blissman <blissman@gmail.com>
2016-03-21 10:57:44 -0400

    set before action in activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,32 @@
 class ActivitiesController < ApplicationController
+
+  before_action :set_activities
+
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where("(owner_id = ? AND owner_type = ?) OR (recipient_id = ? AND recipient_type = ?)", current_user, "User", current_user, "User" )
   end
+
+  def show
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
+    
+  end
+
+  def destroy
+
+    @activities.destroy
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
+  end
+
+  private
+    def set_activities
+      @activities = PublicActivity::Activity.order("created_at desc").where("(owner_id = ? AND owner_type = ?) OR (recipient_id = ? AND recipient_type = ?)", current_user, "User", current_user, "User" )
+    end
+
 end
